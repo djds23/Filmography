9e1cb61f61a793a4a48aa7d020ade2f862455df0
Dean Silfen <dean.silfen@gmail.com>
2022-12-27 13:48:19 -0500

    Initial Commit

---
+++ Filmography/FilmographyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmographyApp.swift
+//  Filmography
+//
+//  Created by Dean Silfen on 12/27/22.
+//
+
+import SwiftUI
+
+@main
+struct FilmographyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
